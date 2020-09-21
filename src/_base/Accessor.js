2b009a663ace78a75b40128f69f9b2f827a4caae
Gavin Fu <gavin.fu@mobinology.com>
2020-09-21 21:27:14 +0800

    Export default

--- src/_base/Accessor.js
+++ src/_base/Accessor.js
@@ -1,41 +1,41 @@
 class Accessor {
-  /**
-   * Get content by "xxx.xxx.xxx" instead of o[xxx][xxx][xxx]
-   * @param {*} obj 
-   * @param {string} accessor 
-   */
-  static Get(obj, accessor){
-    if(!accessor || !obj) return "";
-    let fields = accessor.split('.');
-    let rtn = obj;
+	/**
+	 * Get content by "xxx.xxx.xxx" instead of o[xxx][xxx][xxx]
+	 * @param {*} obj
+	 * @param {string} accessor
+	 */
+	static Get(obj, accessor) {
+		if (!accessor || !obj) return "";
+		let fields = accessor.split(".");
+		let rtn = obj;
 
-    for(let i=0; i<fields.length; i++){
-      if(rtn === null || rtn === undefined) return "";
-      rtn = rtn[fields[i]];
-    }
+		for (let i = 0; i < fields.length; i++) {
+			if (rtn === null || rtn === undefined) return "";
+			rtn = rtn[fields[i]];
+		}
 
-    return rtn;
-  }
+		return rtn;
+	}
 
-  /**
-   * Set content by "xxx.xxx.xxx" instead of o[xxx][xxx][xxx]
-   * @param {*} obj 
-   * @param {string} accessor 
-   * @param {*} value 
-   */
-  static Set(obj, accessor, value){
-    if (value === undefined) return;
-    if (typeof accessor == 'string'){
-      return this.Set(obj, accessor.split('.'), value);
-    }else if (accessor.length == 1 && value !== undefined){
-        return obj[accessor[0]] = value;
-    }else if (accessor.length==0){
-        return obj;
-    }else{
-      if(!obj[accessor[0]]) obj[accessor[0]] = {};
-      return this.Set(obj[accessor[0]], accessor.slice(1), value);
-    }
-  }
+	/**
+	 * Set content by "xxx.xxx.xxx" instead of o[xxx][xxx][xxx]
+	 * @param {*} obj
+	 * @param {string} accessor
+	 * @param {*} value
+	 */
+	static Set(obj, accessor, value) {
+		if (value === undefined) return;
+		if (typeof accessor == "string") {
+			return this.Set(obj, accessor.split("."), value);
+		} else if (accessor.length == 1 && value !== undefined) {
+			return (obj[accessor[0]] = value);
+		} else if (accessor.length == 0) {
+			return obj;
+		} else {
+			if (!obj[accessor[0]]) obj[accessor[0]] = {};
+			return this.Set(obj[accessor[0]], accessor.slice(1), value);
+		}
+	}
 }
 
-module.exports = Accessor
+export default Accessor;
