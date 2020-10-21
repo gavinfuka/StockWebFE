b2bc5ca292787e07dc75708e6b2c8ce4e2c9bf7e
Gavin Fu <gavin.fu@mobinology.com>
2020-10-21 10:41:43 +0800

    UI updates

--- src/Pages/Table/Table.js
+++ src/Pages/Table/Table.js
@@ -18,60 +18,60 @@ import "./Table.css";
 import RefreshIcon from "@material-ui/icons/Refresh";
 import IconButton from "@material-ui/core/IconButton";
 class Table extends Component {
-	constructor(props) {
-		super();
-		this.state = {
-			cssPrefix: "result",
-			data: [],
-		};
-	}
+    constructor(props) {
+        super();
+        this.state = {
+            cssPrefix: "result",
+            data: [],
+        };
+    }
 
-	static async getDerivedStateFromProps(nextProps, prevState) {
-		if (prevState !== nextProps) {
-			return nextProps;
-		}
-	}
-	async componentDidMount() {
-		let data = await this.fectchData();
-		this.setState({ data: data });
-	}
+    static async getDerivedStateFromProps(nextProps, prevState) {
+        if (prevState !== nextProps) {
+            return nextProps;
+        }
+    }
+    async componentDidMount() {
+        let data = await this.fectchData();
+        this.setState({ data: data });
+    }
 
-	fectchData = async () => {
-		let ENV = process.env.NODE_ENV;
-		let { data } = await axios.get(config.Backend[ENV] + "/Result/SMA");
-		return data;
-	};
+    fectchData = async () => {
+        let ENV = process.env.NODE_ENV;
+        let { data } = await axios.get(config.Backend[ENV] + "/Result/SMA");
+        return data;
+    };
 
-	Refresh = async () => {
-		let ENV = process.env.NODE_ENV;
-		let { data } = await axios.get(config.Backend[ENV] + "/Analyse/SMA");
-		return data;
-	};
+    Refresh = async () => {
+        let ENV = process.env.NODE_ENV;
+        let { data } = await axios.get(config.Backend[ENV] + "/Analyse/SMA");
+        return data;
+    };
 
-	renderRefreshBut = () => {
-		return (
-			<IconButton onClick={this.Refresh}>
-				<RefreshIcon />
-			</IconButton>
-		);
-	};
+    renderRefreshBut = () => {
+        return (
+            <IconButton onClick={this.Refresh}>
+                <RefreshIcon />
+            </IconButton>
+        );
+    };
 
-	render() {
-		let { cssPrefix, data } = this.state;
-		// console.log(schema);
-		return (
-			<div>
-				<div>
-					<Header />
-				</div>
-				<div>
-					<div className={cssPrefix + " Date"}>{data._id}</div>
-					{this.renderRefreshBut()}
-					<GTable data={data} schema={schema.table} cssPrefix={cssPrefix} />
-				</div>
-			</div>
-		);
-	}
+    render() {
+        let { cssPrefix, data } = this.state;
+        // console.log(schema);
+        return (
+            <div>
+                <div>
+                    <Header />
+                </div>
+                <div>
+                    <div className={cssPrefix + " Date"}>{data._id}</div>
+                    {this.renderRefreshBut()}
+                    <GTable data={data} schema={schema.table} cssPrefix={cssPrefix} />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Table;
